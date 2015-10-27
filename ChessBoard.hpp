407cdbea820627596438eab281d70a806b1abf54
LaurentGirard <laurent.girard44620@gmail.com>
2015-10-27 12:24:58 +0100

    Modif pas bien depuis master

--- ChessBoard.hpp
+++ ChessBoard.hpp
@@ -11,7 +11,23 @@
 
  class ChessBoard
  {
+ 	private:
 
+ 		Piece chessboard[8][8];
+ 		Player playerA;
+ 		Player playerB;
+
+ 	public:
+
+      	/**
+       * @brief Constructeur, crée un plateau avec deux joueurs, initialisant la position des pièces pour chaque joueur
+       */
+ 		ChessBoard();
+
+ 		/**
+ 		* @brief Destructeur de ChessBoard
+ 		*/
+ 		~ChessBoard();
  };
 
  #endif // __CHESSBOARD_HPP__
