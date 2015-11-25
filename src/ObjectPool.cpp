a7105f8496de42b02929eade47ee3acdc6cecf23
LaurentGirard <laurent.girard44620@gmail.com>
2015-11-25 21:20:04 +0100

    add ObjectPool

---
+++ src/ObjectPool.cpp
@@ -0,0 +1,9 @@
+/**
+* @file ObjectPool.cpp
+* @author P. Sullivan, G.Laurent
+* @since 25/12/2015
+* @brief implémentation des méthodes de la classe ObjectPool
+*
+**/
+
+#include "ObjectPool.hpp"
---
+++ src/ObjectPool.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file Chess.hpp
+ * @author P. Sullivan, G.Laurent
+ * @since 25/12/2015
+ * @brief Définition de la classe ObjectPool permettant à un joueur de prendre ou remettre ses pièces depuis une pool
+ *
+**/
+
+#ifndef __OBJECTPOOL_HPP__
+#define __OBJECTPOOL_HPP__
+
+class ObjectPool{
+
+};
+
+#endif // __OBJECTPOOL_HPP__
